a0cb18b771f8b4ea0e6f79630051a2475714b8dd
Marek <asdas@wp.pl>
2018-11-21 18:02:28 +0100

    praca domowa

---
+++ 357(2).py
@@ -0,0 +1,6 @@
+a = int(input("Wprowadz liczbe: "))
+
+if(a % 3 == 0 and a % 5 == 0 and a % 7 == 0):
+    print("Liczba jest podzielna przez 3, 5 i 7 na raz.")
+else:
+    print("Liczba nie jest podzielna przez 3, 5, i 7 na raz.")
---
+++ 357.py
@@ -0,0 +1,8 @@
+a = int(input("Wprowadz liczbe: "))
+
+if(a % 3 == 0):
+    print("Liczba jest podzielna przez 3.")
+elif(a % 5 == 0):
+    print("Liczba jest podzielna przez 5.")
+elif(a % 7 == 0):
+    print("Liczba jest podzielna przez 7.")
